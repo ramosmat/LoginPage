d771910ccbb771aaa0f53fc9ce6ba12c8eb1641b
Matheus <matheusramosbi.13@gmail.com>
2023-10-26 17:02:51 -0300

    +JS functions

---
+++ script.js
@@ -0,0 +1,12 @@
+function esqueci(){
+    alert("Só não pode esquecer de ver minhas redes ali embaixo! haha")
+}
+
+function entrar(){
+    alert("🎉🎊🎉🎊")
+}
+
+function ainda(){
+    alert("Relaxa, nada vai ser salvo 😉")
+}
+

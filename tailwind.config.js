3f26f1f67504f1cb5f6c7bc52bb9e17c8e14fa51
Matheus <matheusramosbi.13@gmail.com>
2023-11-16 14:00:50 -0300

    adjustment on notification entry

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["DM Sans", "sans-serif"]
+        sans: ["DM Sans", "sans-serif"],
       },
       animation: {
         notification: "notification-enter 0.2s linear",
       },
       keyframes: {
         "notification-enter": {
-          "0%": {transform: "translateX(18rem)", opacity:0},
-          "100%": {transform: "translateX(0rem)", opacity:100}
+          "0%": {transform: "translateX(18rem)", opacity: 0},
+          "100%": {transform: "translateX(0rem)", opacity: 100},
         }
       }
     },
